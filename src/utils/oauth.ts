deddcc1d4ccd08122abb8598d7ce193b44d2ad10
Liberatorist <Liberatorist@gmail.com>
2025-04-24 23:47:13 +0200

    surely dynamic oauth callback urls will work now

--- src/utils/oauth.ts
+++ src/utils/oauth.ts
@@ -1,21 +1,21 @@
 import { oauthApi } from "@client/client";
 
+export function getCallbackUrl(provider: "discord" | "twitch" | "poe") {
+  let callbackUrl = window.location.origin;
+  if (callbackUrl === "http://localhost") {
+    // oauth providers require https
+    callbackUrl = "https://redirectmeto.com/" + callbackUrl;
+  }
+  callbackUrl += "/auth/" + provider + "/callback";
+  return callbackUrl;
+}
+
 export function redirectOauth(
   provider: "discord" | "twitch" | "poe",
   latestUrl: string
-): () => Promise<void> {
-  // hack to enable dynamic redirect uris depending on where the frontend is hosted
+): () => Promise<void | Window | null> {
   return () =>
-    oauthApi.oauthRedirect(provider, latestUrl).then((urlString) => {
-      const url = new URL(urlString);
-      let redirect_uri = window.location.origin;
-      if (redirect_uri === "http://localhost") {
-        redirect_uri = "https://redirectmeto.com/" + redirect_uri;
-      }
-      url.searchParams.set(
-        "redirect_uri",
-        redirect_uri + "/auth/" + provider + "/callback"
-      );
-      window.open(url, "_self");
-    });
+    oauthApi
+      .oauthRedirect(provider, getCallbackUrl(provider), latestUrl)
+      .then((urlString) => window.open(urlString, "_self"));
 }
