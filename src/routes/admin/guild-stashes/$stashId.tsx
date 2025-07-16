13c9ccae2f1d31c9669196fefc4a3651d4eff3f5
Liberatorist <Liberatorist@gmail.com>
2025-07-17 00:04:52 +0200

    improve display item view

--- src/routes/admin/guild-stashes/$stashId.tsx
+++ src/routes/admin/guild-stashes/$stashId.tsx
@@ -97,55 +97,79 @@ function RouteComponent() {
         closeOnOutsideClick={true}
       >
         <div className="">
-          <div className="divider m-0"></div>
+          {selectedItem?.influences && (
+            <>
+              <div className="flex flex-row gap-2 mb-2">
+                Influences:{" "}
+                {Object.keys(selectedItem?.influences || {}).map(
+                  (influence) => (
+                    <span key={influence}>{influence.toUpperCase()}</span>
+                  )
+                )}
+              </div>
+            </>
+          )}
+          {(selectedItem?.properties?.length || 0) > 0 && (
+            <div className="flex flex-col">
+              {selectedItem?.properties?.map((prop) => {
+                if (prop.displayMode === 3) {
+                  const values = prop.values?.map((v) => v[0]);
+                  let name = prop.name || "";
+                  values?.forEach((value, i) => {
+                    name = name.replace(`{${i}}`, value);
+                  });
 
-          <div className="flex flex-col">
-            {selectedItem?.properties?.map((prop) => {
-              if (prop.displayMode === 3) {
-                const values = prop.values?.map((v) => v[0]);
-                let name = prop.name || "";
-                values?.forEach((value, i) => {
-                  name = name.replace(`{${i}}`, value);
-                });
-
-                return <span className="text-base-content/80">{name}</span>;
-              }
-              return (
-                <p>
-                  <span className="text-base-content/80">{prop.name}:</span>{" "}
-                  <span className="text-info font-semibold">
-                    {prop.values?.[0]?.[0] || prop.values?.[0]?.[1]}{" "}
-                    {prop.displayMode == 1 ? prop.values?.[0]?.[1] : ""}
-                  </span>
-                </p>
-              );
-            })}
-          </div>
+                  return <span className="text-base-content/80">{name}</span>;
+                }
+                return (
+                  <p>
+                    <span className="text-base-content/80">{prop.name}:</span>{" "}
+                    <span className="text-magic">
+                      {prop.values?.[0]?.[0] || prop.values?.[0]?.[1]}{" "}
+                      {prop.displayMode == 1 ? prop.values?.[0]?.[1] : ""}
+                    </span>
+                  </p>
+                );
+              })}
+            </div>
+          )}
           {selectedItem?.ilvl !== 0 && (
             <>
               <div className="divider m-0"></div>
               <span className="text-base-content/80">Item Level:</span>{" "}
-              <span className="text-info font-semibold">
-                {selectedItem?.ilvl}
-              </span>
+              <span className="text-magic">{selectedItem?.ilvl}</span>
             </>
           )}
           {selectedItem?.implicitMods && (
             <>
               <div className="divider m-0"></div>
-              <div className="flex flex-col text-info font-semibold">
+              <div className="flex flex-col text-magic">
                 {selectedItem.implicitMods.map((mod, idx) => (
                   <p key={idx}>{mod}</p>
                 ))}
               </div>
             </>
           )}
-          {selectedItem?.explicitMods && (
+          {(selectedItem?.explicitMods ||
+            selectedItem?.craftedMods ||
+            selectedItem?.fracturedMods) && (
             <>
               <div className="divider m-0"></div>
-              <div className="flex flex-col text-info font-semibold">
-                {selectedItem.explicitMods.map((mod, idx) => (
-                  <p key={idx}>{mod}</p>
+              <div className="flex flex-col">
+                {selectedItem.explicitMods?.map((mod, idx) => (
+                  <span className="text-magic" key={idx}>
+                    {mod}
+                  </span>
+                ))}
+                {selectedItem.craftedMods?.map((mod, idx) => (
+                  <p className="text-crafted" key={idx}>
+                    {mod}
+                  </p>
+                ))}
+                {selectedItem.fracturedMods?.map((mod, idx) => (
+                  <p className="text-fractured" key={idx}>
+                    {mod}
+                  </p>
                 ))}
               </div>
             </>
