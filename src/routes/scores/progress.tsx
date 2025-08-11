5bfbad9fadb4f70fc083b7f9573d59735adf3370
Liberatorist <fabian.mueller77@gmail.com>
2025-08-11 18:44:58 +0200

    add graph to progress page

--- src/routes/scores/progress.tsx
+++ src/routes/scores/progress.tsx
@@ -1,4 +1,4 @@
-import { ObjectiveType, Score, Team } from "@client/api";
+import { ObjectiveType, Score, ScoringMethod, Team } from "@client/api";
 import { ObjectiveIcon } from "@components/objective-icon";
 import Table from "@components/table";
 import { TeamName } from "@components/team-name";
@@ -8,10 +8,12 @@ import { createFileRoute } from "@tanstack/react-router";
 import { ColumnDef } from "@tanstack/react-table";
 import { GlobalStateContext } from "@utils/context-provider";
 import { flatMap, timeSort } from "@utils/utils";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { getDeltaTimeBetween } from "@utils/time";
+import { AlignedData } from "uplot";
+import UplotReact from "uplot-react";
 dayjs.extend(relativeTime);
 
 export const Route = createFileRoute("/scores/progress")({
@@ -24,10 +26,14 @@ type ScoreRow = {
 } & Score;
 
 function RouteComponent() {
-  const { currentEvent, scores } = useContext(GlobalStateContext);
+  const { currentEvent, scores, preferences } = useContext(GlobalStateContext);
+  const plotRef = useRef<HTMLDivElement>(null);
   const [timeFormat, setTimeFormat] = useState<"relative" | "absolute">(
     "absolute"
   );
+  const [deviationFromAvg, setDeviationFromAvg] = useState(false);
+  const fontColor = preferences.theme === "dark" ? "white" : "black";
+
   const [onlyShowRanked, setOnlyShowRanked] = useState(false);
   const teamMap = currentEvent.teams.reduce(
     (acc, team) => {
@@ -79,8 +85,120 @@ function RouteComponent() {
         ...score,
       }))
     )
-    .filter((s) => s.finished && s.points > 0)
-    .sort(timeSort<Score, "timestamp">("timestamp", "desc"));
+    .filter((s) => s.points > 0);
+
+  const timestamp2Scores: Record<string, Record<number, number>> = {};
+  const currentVal = currentEvent.teams.reduce(
+    (acc, team) => {
+      acc[team.id] = 0;
+      return acc;
+    },
+    {} as Record<number, number>
+  );
+  currentVal[0] = 0;
+
+  for (const scoreRow of scoreRows
+    .filter(
+      (s) =>
+        s.objective.scoring_preset?.scoring_method !=
+        ScoringMethod.POINTS_FROM_VALUE
+    )
+    .sort(timeSort<ScoreRow, "timestamp">("timestamp", "asc"))) {
+    if (!scoreRow.team) continue;
+    currentVal[scoreRow.team.id] += scoreRow.points;
+    currentVal[0] =
+      currentEvent.teams.reduce(
+        (acc, team) => acc + (currentVal[team.id] || 0),
+        0
+      ) / currentEvent.teams.length;
+    if (!timestamp2Scores[scoreRow.timestamp]) {
+      timestamp2Scores[scoreRow.timestamp] = { ...currentVal };
+    }
+    timestamp2Scores[scoreRow.timestamp][scoreRow.team.id] =
+      currentVal[scoreRow.team.id];
+  }
+  const timestamps: number[] = [];
+  const teamData = currentEvent.teams.reduce(
+    (acc, team) => {
+      acc[team.id] = [];
+      return acc;
+    },
+    {} as Record<number, number[]>
+  );
+  teamData[0] = []; // For the average score
+  const eventStart = new Date(currentEvent.event_start_time).getTime() / 1000;
+  const eventEnd = new Date(currentEvent.event_end_time).getTime() / 1000;
+  for (const [timestamp, scores] of Object.entries(timestamp2Scores)) {
+    const ts = new Date(timestamp).getTime() / 1000;
+    if (ts < eventStart || ts > eventEnd) continue;
+
+    timestamps.push(ts);
+    for (const team of currentEvent.teams) {
+      let val = scores[team.id] || 0;
+      if (deviationFromAvg) {
+        val = Math.round(val - scores[0]);
+      }
+      teamData[team.id].push(val);
+    }
+  }
+  const data: AlignedData = [
+    new Float64Array(timestamps),
+    ...currentEvent.teams
+      .sort((a, b) => a.id - b.id)
+      .map((team) => new Float64Array(teamData[team.id])),
+  ];
+  const options: uPlot.Options = {
+    title: "Progression (without P.O. / Culmulative Delve)",
+    width: 800,
+    height: 800,
+    legend: { show: true },
+    axes: [
+      {
+        side: 2,
+        scale: "x",
+        ticks: { size: 0 },
+        label: "Time",
+        stroke: fontColor,
+      },
+      {
+        label: "Points",
+        labelFont: "16px sans-serif",
+        side: 3,
+        scale: "points",
+        stroke: fontColor,
+      },
+    ],
+    series: [
+      {
+        label: "time",
+        points: { show: false },
+        scale: "x",
+      },
+      ...currentEvent.teams
+        .sort((a, b) => a.id - b.id)
+        .map((team) => ({
+          label: team.name,
+          stroke: team.color,
+          width: 2,
+          points: {
+            size: 8,
+            stroke: team.color,
+            fill: team.color,
+          },
+          scale: "points",
+        })),
+    ],
+    scales: {
+      x: {
+        time: true,
+        range: (self, initMin, initMax) => [
+          self.scales.x.min || initMin,
+          self.scales.x.max || initMax,
+        ],
+      },
+    },
+  };
+
   const columns: ColumnDef<ScoreRow>[] = [
     {
       accessorKey: "id",
@@ -211,7 +329,7 @@ function RouteComponent() {
   ];
 
   return (
-    <div className="flex flex-col gap-4 py-4">
+    <div className="flex flex-col gap-4">
       <fieldset className="fieldset bg-base-100 border-base-300 rounded-box border p-4 flex flex-row gap-10 w-80">
         <legend className="fieldset-legend"></legend>
         <label className="label w-30">
@@ -237,17 +355,44 @@ function RouteComponent() {
       </fieldset>
       <Table
         columns={columns}
-        data={scoreRows.filter((s) => {
-          if (s.objective.objective_type == ObjectiveType.TEAM) {
-            return false;
-          }
-          if (onlyShowRanked) {
-            return s.rank > 0;
-          }
-          return true;
-        })}
+        data={scoreRows
+          .filter((s) => {
+            if (s.objective.objective_type == ObjectiveType.TEAM) {
+              return false;
+            }
+            if (onlyShowRanked) {
+              return s.rank > 0;
+            }
+            return true;
+          })
+          .sort(timeSort<ScoreRow, "timestamp">("timestamp", "desc"))}
         className="h-[70vh]"
       ></Table>
+      <div className="h-[1000px] bg-base-300 p-4 rounded-box mt-4">
+        <fieldset className="fieldset bg-base-200 absolute p-2 px-4 rounded-box ml-8">
+          <label className="label text-highlight-content">
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={deviationFromAvg}
+              onChange={(e) => setDeviationFromAvg(e.target.checked)}
+            />
+            <span className="label-text">Show Deviation from Average</span>
+          </label>
+        </fieldset>
+        <div ref={plotRef} className="h-full">
+          <UplotReact
+            options={options}
+            data={data}
+            onCreate={(chart) => {
+              chart.setSize({
+                width: plotRef.current?.clientWidth || 800,
+                height: (plotRef.current?.clientHeight || 400) - 100,
+              });
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
