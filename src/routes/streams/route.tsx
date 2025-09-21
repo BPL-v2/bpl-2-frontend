bddf15c6cc79a264b144d6096ba688ee00feb0ca
Liberatorist <liberatorist@gmail.com>
2025-09-21 21:55:43 +0200

    add character portraits to twitch streams

--- src/routes/streams/route.tsx
+++ src/routes/streams/route.tsx
@@ -2,9 +2,22 @@ import { createFileRoute, Link, Outlet } from "@tanstack/react-router";
 import { useContext } from "react";
 import { TwitchStreamEmbed } from "@components/twitch-stream";
 import { GlobalStateContext } from "@utils/context-provider";
-import { EventStatus, Team } from "@client/api";
-import { useGetEventStatus, useGetStreams, useGetUsers } from "@client/query";
+import {
+  EventStatus,
+  GameVersion,
+  LadderEntry,
+  Team,
+  Event,
+} from "@client/api";
+import {
+  useGetEventStatus,
+  useGetLadder,
+  useGetStreams,
+  useGetUsers,
+} from "@client/query";
 import { usePageSEO } from "@utils/use-seo";
+import { ascendancies, phreciaMapping, poe2Mapping } from "@mytypes/ascendancy";
+import { twMerge } from "tailwind-merge";
 
 export const Route = createFileRoute("/streams")({
   component: TwitchPage,
@@ -26,11 +39,81 @@ function teamSort(
   };
 }
 
+function CharacterPortrait({
+  ladderEntry,
+  currentEvent,
+}: {
+  ladderEntry?: LadderEntry;
+  currentEvent: Event;
+}) {
+  const character = ladderEntry?.character;
+  if (!ladderEntry || !character) return null;
+  let ascendancyName = character?.ascendancy;
+  let ascendancyObj;
+  if (currentEvent.game_version === GameVersion.poe2) {
+    ascendancyName = poe2Mapping[character.ascendancy] || character.ascendancy;
+    ascendancyObj = ascendancies[GameVersion.poe2][ascendancyName];
+  } else {
+    ascendancyObj =
+      ascendancies[GameVersion.poe1][
+        phreciaMapping[character.ascendancy] || character.ascendancy
+      ];
+  }
+
+  return (
+    <Link
+      to={"/profile/$userId/$eventId/$characterId"}
+      params={{
+        characterId: character?.id,
+        userId: ladderEntry.user_id || 0,
+        eventId: currentEvent.id,
+      }}
+      className={
+        "bg-base-300 cursor-pointer select-none flex flex-row gap-4 rounded-t-box border-4 items-center p-1 "
+      }
+      activeProps={{
+        className: "border-primary shadow-2xl",
+      }}
+      inactiveProps={{
+        className: "border-transparent",
+      }}
+    >
+      <img
+        src={ascendancyObj.thumbnail}
+        className="rounded-full size-14"
+        alt={ascendancyName}
+      />
+      <div className="text-lg text-left">
+        <p> {character.name}</p>
+        <div className="flex flex-row gap-2">
+          <span>Level {character.level}</span>
+          <span className={twMerge("font-bold", ascendancyObj.classColor)}>
+            {ascendancyName}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
 function TwitchPage() {
   usePageSEO("streams");
   const { currentEvent } = useContext(GlobalStateContext);
   const { users } = useGetUsers(currentEvent.id);
   const { eventStatus } = useGetEventStatus(currentEvent.id);
+  const { ladder = [] } = useGetLadder(currentEvent.id);
+  const userToCharacter = ladder.reduce(
+    (acc, entry) => {
+      if (entry.user_id && entry.character_name) {
+        if (acc[entry.user_id] && acc[entry.user_id].level > entry.level) {
+          return acc;
+        }
+        acc[entry.user_id] = entry;
+      }
+      return acc;
+    },
+    {} as Record<number, LadderEntry>
+  );
   const {
     data: streams,
     isPending: streamsPending,
@@ -60,16 +143,30 @@ function TwitchPage() {
                 )
               )
               .sort((a, b) => (b.viewer_count || 0) - (a.viewer_count || 0))
-              .map((stream) => (
-                <Link
-                  to={"/streams/$twitchAccount"}
-                  params={{ twitchAccount: stream.user_login ?? "" }}
-                  className="cursor-pointer rounded-field border-2"
-                  activeProps={{ className: "border-primary" }}
-                >
-                  <TwitchStreamEmbed stream={stream} width={340} height={170} />
-                </Link>
-              ))}
+              .map((stream) => {
+                return (
+                  <div>
+                    <CharacterPortrait
+                      ladderEntry={
+                        userToCharacter[stream.backend_user_id || -1]
+                      }
+                      currentEvent={currentEvent}
+                    />
+                    <Link
+                      to={"/streams/$twitchAccount"}
+                      params={{ twitchAccount: stream.user_login ?? "" }}
+                      className="cursor-pointer bg-base-300 "
+                      activeProps={{ className: "border-primary" }}
+                    >
+                      <TwitchStreamEmbed
+                        stream={stream}
+                        width={340}
+                        height={170}
+                      />
+                    </Link>
+                  </div>
+                );
+              })}
           </div>
         </div>
       ))}
